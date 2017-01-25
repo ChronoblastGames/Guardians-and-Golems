ae0d4b719c2d3b39760c6c3d08f7724d88f75593
Chronoblast -Class- <ChronoblastGamesOfficial@gmail.com>
2017-01-25 17:24:50 -0500

    Does this fix it?

---
+++ Assets/Scripts/Entities/Player/Golem/Abilities/GolemAbility.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public enum DamageType {
+    FIRE,
+    ICE,
+    WIND,
+    EARTH
+}
+
+public abstract class GolemAbility : MonoBehaviour
+{
+
+    [Header("Ability Attributes")]
+    public DamageType damageType;
+
+    [Header("Range Attributes")]
+    public float spawnDistanceFromPlayer;
+    public float rangeDistance;
+
+    public bool isMelee;
+    public bool isRanged;
+
+    [Header("Use Attributes")]
+    public float healthCost;
+    public float manaCost;
+
+    [Header("Has Effect")]
+    public bool canStun;
+    public bool canSlow;
+    public bool canDrainHealth;
+    public bool canDrainMana;
+    public bool canBlind;
+
+
+    public virtual void CastAbility()
+    {
+        Debug.Log("Base Method Called");
+    }
+
+}
+
+
---
+++ Assets/Scripts/Entities/Player/Golem/Logic/GolemPlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class GolemPlayerController : GolemStats 
+{
+    private GolemInputManager golemInputManager;
+
+    private Rigidbody playerRigidbody;
+
+    private float xAxis;
+    private float zAxis;
+
+    [Header("Player Ability Attributes")]
+    public GolemAbility[] Abilities;
+
+    [Header("Player Turning Attributes")]
+    public float turnSpeed;
+
+	void Start () 
+    {
+        PlayerSetup();
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        GatherInput();
+    }
+
+    void PlayerSetup()
+    {
+        golemInputManager = GetComponent<GolemInputManager>();
+
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void GatherInput()
+    {
+        xAxis = golemInputManager.xAxis;
+        zAxis = golemInputManager.zAxis;
+
+        Move(xAxis, zAxis);
+    }
+
+    void Move(float xAxis, float zAxis)
+    {
+        if (xAxis != 0 || zAxis != 0)
+        {
+            Vector3 moveVec = new Vector3(xAxis, 0, zAxis) * baseMovementSpeed * Time.deltaTime;
+
+            if (moveVec.magnitude > 1)
+            {
+                moveVec.Normalize();
+            }
+
+            Turn(moveVec);
+
+            playerRigidbody.MovePosition(transform.position + moveVec);
+        }       
+    }
+
+    void Turn(Vector3 lookVec)
+    {
+        Quaternion desiredRotation = Quaternion.LookRotation(lookVec);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, Time.deltaTime * turnSpeed);
+    }       
+}
