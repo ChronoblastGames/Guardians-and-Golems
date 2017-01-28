5925007a3ebd1e5bf34031f1b9c830cca4a142dd
Phlares <ryon.everett@gmail.com>
2017-01-28 01:55:41 -0500

    Started working the input buffer script
    added command class and input buffer class.

---
+++ Assets/Scripts/Systems/InputBuffering/InputBuffer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command{
+
+
+    Command(){
+
+    }
+
+    virtual void Execute(){
+
+
+    }
+
+}
+
+public class InputBuffer : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    Command BufferInput(){
+
+    }
+    
+}
