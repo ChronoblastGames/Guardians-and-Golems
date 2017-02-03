8ae69680a44e015ccd4226946659317f81e5bbd3
Phlares <ryon.everett@gmail.com>
2017-02-03 00:36:36 -0500

    More work on input buffer
    added some command classes, will ahve to refactor and edit after

--- Assets/Scripts/Systems/InputBuffering/InputBuffer.cs
+++ Assets/Scripts/Systems/InputBuffering/InputBuffer.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class Command{
 
 
-    Command(){
+    
+   public Command(){
 
     }
 
-    public virtual void Execute(){
-
-
-    }
+    public virtual void Execute(GameObject agentCommander) {}
 
 }
+public class MoveCommand : Command {
+    public override void Execute(GameObject agentCommander) { MovePlayer(agentCommander); }
+    
+   public void MovePlayer(GameObject p_agentCommander) { Debug.Log("Command MovePlayer() called"); }
+
+}
+
+public class UseAbilityCommand : Command
+{
+    public override void Execute(GameObject agentCommander) { UseAbility(agentCommander); }
+    public void UseAbility(GameObject p_agentCommander) { Debug.Log("Command UseAbility() called"); }
+
+}
+public class ApplyEffectCommand : Command
+{
+    public override void Execute(GameObject agentCommander) { ApplyEffect(agentCommander); }
+    public void ApplyEffect(GameObject p_agentCommander) { Debug.Log("Command ApplyEffect() called"); }
+
+}
+
 
 public class InputBuffer : MonoBehaviour {
 
