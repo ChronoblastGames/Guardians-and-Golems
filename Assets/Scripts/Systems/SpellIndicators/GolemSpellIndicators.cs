02e81475734cc544dd5b8fee6018c164c62fa54a
Chron <ChronoblastGamesOfficial@gmail.com>
2017-02-20 14:19:49 -0500

    Added other Projector Materials and Started work on Aim indicator adapting to cast spell.

--- Assets/Scripts/Systems/SpellIndicators/GolemSpellIndicators.cs
+++ Assets/Scripts/Systems/SpellIndicators/GolemSpellIndicators.cs
@@ -1,6 +1,15 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+public enum IndicatorType
+{
+    NONE,
+    ARROW,
+    CONE,
+    CIRCLE,
+    POINT
+}
+
 public class GolemSpellIndicators : MonoBehaviour 
 {
     private GolemInputManager golemInput;
@@ -9,7 +18,17 @@ public class GolemSpellIndicators : MonoBehaviour
     private float aimZAxis;
 
     [Header("Golem Spell Indicator")]
+    public IndicatorType spellIndicatorType;
+
     public GameObject spellIndicator;
+
+    public bool isSpellIndicatorEnabled;
+
+    [Header("Golem Spell Materials")]
+    public Material spellIndicatorCircle;
+    public Material spellIndicatorPoint;
+    public Material spellIndicatorArrow;
+    public Material spellIndicatorCone;
 
     private void Start()
     {
@@ -28,25 +47,48 @@ public class GolemSpellIndicators : MonoBehaviour
 
     void ManageAimIndicator()
     {
-        aimXAxis = golemInput.aimXAxis;
-        aimZAxis = golemInput.aimZAxis;
-
-        spellIndicator.transform.position = golemInput.transform.position;
-
-        if (aimXAxis != 0 || aimZAxis != 0)
+        if (isSpellIndicatorEnabled)
         {
-            spellIndicator.SetActive(true);
+            switch(spellIndicatorType)
+            {
+                case IndicatorType.ARROW:
 
-            Vector2 aimVector = new Vector2(aimXAxis, aimZAxis);
-            float angle = Mathf.Atan2(aimZAxis, aimXAxis) * Mathf.Rad2Deg;
+                    aimXAxis = golemInput.aimXAxis;
+                    aimZAxis = golemInput.aimZAxis;
 
-            angle -= 90f;
+                    spellIndicator.transform.position = golemInput.transform.position + new Vector3(0, 10, 0);
 
-            spellIndicator.transform.localRotation = Quaternion.Euler(0, 0, angle);
-        }
-        else
-        {
-            spellIndicator.SetActive(false);
-        }
+                    if (aimXAxis != 0 || aimZAxis != 0)
+                    {
+                        spellIndicator.SetActive(true);
+
+                        Vector2 aimVector = new Vector2(aimXAxis, aimZAxis);
+                        float angle = Mathf.Atan2(aimZAxis, aimXAxis) * Mathf.Rad2Deg;
+
+                        angle -= 90f;
+
+                        spellIndicator.transform.localRotation = Quaternion.Euler(0, 0, angle);
+                    }
+                    else
+                    {
+                        spellIndicator.SetActive(false);
+                    }
+
+                    break;
+
+                case IndicatorType.CIRCLE:
+                    break;
+
+                case IndicatorType.CONE:
+                    break;
+
+                case IndicatorType.POINT:
+                    break;
+
+                case IndicatorType.NONE:
+                    break;
+            }
+
+        }      
     }
 }
