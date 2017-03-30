61fd10f64ca356fb88ad8059441e6a000379df93
Chronoblast -Class- <ChronoblastGamesOfficial@gmail.com>
2017-03-29 22:05:42 -0400

    Baked lightning of main scene, added Oneshot Animator Functionality

---
+++ Assets/Scripts/Utility/PlayAnimationAfterSeconds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayAnimationAfterSeconds : MonoBehaviour
+{
+    public Animator targetAnimator;
+
+    public float waitTime;
+
+    void Start()
+    {
+
+    }
+
+    private IEnumerator PlayAnimation()
+    {
+        if (waitTime > 0)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            targetAnimator.SetTrigger("Trigger");
+        }
+    }
+}
