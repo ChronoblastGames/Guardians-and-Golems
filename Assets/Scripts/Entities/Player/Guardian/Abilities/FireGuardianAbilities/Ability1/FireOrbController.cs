e7f65d0d94ae5ac5c6e6177481cfdf529b0698ef
Chronoblast -Class- <ChronoblastGamesOfficial@gmail.com>
2017-04-04 17:49:46 -0400

    Started work on Firebola Guardian Ability

---
+++ Assets/Scripts/Entities/Player/Guardian/Abilities/FireGuardianAbilities/Ability1/FireOrbController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FireOrbController : MonoBehaviour
+{
+    public AbilityValues fireballAbilityValues;
+
+    private Firebola fireBola;
+
+    void Start()
+    {
+        fireBola = transform.parent.parent.GetComponent<Firebola>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Hit Something");
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Walls"))
+        {
+            fireBola.fireballList.Remove(gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.layer == LayerMask.NameToLayer("GolemRed"))
+        {
+            if (other.gameObject.CompareTag("GolemRed"))
+            {
+                other.gameObject.GetComponent<GolemResources>().TakeDamage(fireballAbilityValues.damageAmount, fireballAbilityValues.damageType, fireballAbilityValues.statusEffect, fireballAbilityValues.effectStrength, fireballAbilityValues.effectTime, gameObject);
+                fireBola.fireballList.Remove(gameObject);
+                Destroy(gameObject);
+            }
+        }
+        else if (gameObject.layer == LayerMask.NameToLayer("GolemBlue"))
+        {
+            if (other.gameObject.CompareTag("GolemBlue"))
+            {
+                other.gameObject.GetComponent<GolemResources>().TakeDamage(fireballAbilityValues.damageAmount, fireballAbilityValues.damageType, fireballAbilityValues.statusEffect, fireballAbilityValues.effectStrength, fireballAbilityValues.effectTime, gameObject);
+                fireBola.fireballList.Remove(gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
